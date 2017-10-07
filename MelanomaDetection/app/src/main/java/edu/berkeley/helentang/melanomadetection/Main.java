f640df1384c2c3d37d7dca17ad3c5c4470388e3d
helen <helen@airbears2-10-142-149-78.airbears2.1918.berkeley.edu>
2017-10-07 11:40:41 -0700

    Add directory

---
+++ MelanomaDetection/app/src/main/java/edu/berkeley/helentang/melanomadetection/Main.java
@@ -0,0 +1,19 @@
+package edu.berkeley.helentang.melanomadetection;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+
+public class Main extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.main);
+    }
+
+    /** Called when the user taps the Send button */
+    public void sendMessage(View view) {
+        // Do something in response to button
+    }
+}
