6d0b1755f15dc09c145afd3e8246b8960bbdea3f
Chris Bender <=>
2017-10-08 06:19:42 -0700

    pictures

--- SimpleCamera/app/src/main/java/edu/berkeley/jamesyc23/MainActivity.java
+++ SimpleCamera/app/src/main/java/edu/berkeley/jamesyc23/MainActivity.java
@@ -17,124 +17,145 @@ import android.widget.FrameLayout;
 import android.widget.ImageButton;
 import android.widget.RelativeLayout;
 
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.FileOutputStream;
 import java.io.IOException;
+
+import static android.provider.MediaStore.Files.FileColumns.MEDIA_TYPE_IMAGE;
 
 
 public class MainActivity extends AppCompatActivity {
 
     private final static String TAG = "MainActivity";
 
-    private Camera mCamera = null;
-//    private CameraView mCameraView = null;
-
-    private SurfaceView cameraView;
+    private Camera mCamera;
+    private CameraPreview mPreview;
+    private Camera.PictureCallback mPicture;
 
     private Button takePhotoButton;
     private Button analyzeButton;
     private Button retakePhotoButton;
+
+    private Button profileButton;
+    private Button settingsButton;
+    private Button practitionerButton;
+    private Button faqButton;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-
-        cameraView = (SurfaceView) findViewById(R.id.camera_view);
-
         takePhotoButton = (Button) findViewById(R.id.take_photo);
         analyzeButton = (Button) findViewById(R.id.analyze);
         retakePhotoButton = (Button) findViewById(R.id.retake_photo);
 
+        takePhotoButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                takePhoto();
+            }
+        });
+
+        analyzeButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+
+            }
+        });
+
+        retakePhotoButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                retakePhoto();
+            }
+        });
+
+        profileButton = (Button) findViewById(R.id.button_profile);
+        settingsButton = (Button) findViewById(R.id.button_settings);
+        practitionerButton = (Button) findViewById(R.id.button_practitioner);
+        faqButton = (Button) findViewById(R.id.button_faq);
+
+        profileButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(MainActivity.this, DisplayProfileActivity.class);
+                startActivity(intent);
+            }
+        });
+
+        settingsButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(MainActivity.this, DisplaySettingsActivity.class);
+                startActivity(intent);
+            }
+        });
+
+        practitionerButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(MainActivity.this, DisplayPractitionerActivity.class);
+                startActivity(intent);
+            }
+        });
+
+        faqButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(MainActivity.this, DisplayFaqActivity.class);
+                startActivity(intent);
+            }
+        });
+
+
+        mPicture = new Camera.PictureCallback() {
+
+            @Override
+            public void onPictureTaken(byte[] data, Camera camera) {
+
+                Log.i(TAG, data.toString());
+
+//                File pictureFile = null;
+//
+//                try {
+//                    FileOutputStream fos = new FileOutputStream(pictureFile);
+//                    fos.write(data);
+//                    fos.close();
+//                } catch (FileNotFoundException e) {
+//                    Log.d(TAG, "File not found: " + e.getMessage());
+//                } catch (IOException e) {
+//                    Log.d(TAG, "Error accessing file: " + e.getMessage());
+//                }
+            }
+        };
+
         openFeed();
 
-        //btn to close the application
-//        ImageButton imgClose = (ImageButton)findViewById(R.id.imgClose);
-//        imgClose.setOnClickListener(new View.OnClickListener() {
-//            @Override
-//            public void onClick(View view) {
-//                System.exit(0);
-//            }
-//        });
     }
 
     public void openFeed() {
 
         if (mCamera == null) {
             try {
-                mCamera = Camera.open();//you can use open(int) to use different cameras
+                mCamera = Camera.open(Camera.CameraInfo.CAMERA_FACING_BACK);//you can use open(int) to use different cameras
             } catch (Exception e) {
                 Log.d("ERROR", "Failed to get camera: " + e.getMessage());
             }
         }
+        mCamera.setDisplayOrientation(90);
 
-//        Camera.Parameters parameters = mCamera.getParameters();
-//        for (Camera.Size size : parameters.getSupportedPreviewSizes()){
-//            Log.i(TAG, "Supported size: w = " + size.width  + ", h = " + size.height);
-//        }
-//        parameters.setPreviewSize(1088, 1088);
-//        mCamera.setParameters(parameters);
-
-        Camera camera = Camera.open(Camera.CameraInfo.CAMERA_FACING_BACK);
-        Camera.Parameters camParams = camera.getParameters();
-
-        // Find a preview size that is at least the size of our IMAGE_SIZE
-        Camera.Size previewSize = camParams.getSupportedPreviewSizes().get(0);
-        for (Camera.Size size : camParams.getSupportedPreviewSizes()) {
-            if (size.width >= 1024 && size.height >= 1024) {
-                previewSize = size;
-                break;
-            }
-        }
-        camParams.setPreviewSize(previewSize.width, previewSize.height);
-
-        // Try to find the closest picture size to match the preview size.
-        Camera.Size pictureSize = camParams.getSupportedPictureSizes().get(0);
-        for (Camera.Size size : camParams.getSupportedPictureSizes()) {
-            if (size.width == previewSize.width && size.height == previewSize.height) {
-                pictureSize = size;
-                break;
-            }
-        }
-        camParams.setPictureSize(pictureSize.width, pictureSize.height);
-
-//        mCameraView = new CameraView(this, mCamera);//create a SurfaceView to show camera data
-//        cameraView.addView(mCameraView);//add the SurfaceView to the layout
-
-//        Log.i(TAG, "" + mCamera.getParameters().getPreviewSize().width);
-//        Log.i(TAG, "" + mCamera.getParameters().getPreviewSize().height);
-//
-//        Log.i(TAG, "w = " + mCameraView.getWidth());
-//        Log.i(TAG, "h = " + mCameraView.getHeight());
+        mPreview = new CameraPreview(this, mCamera);
+        RelativeLayout preview = (RelativeLayout) findViewById(R.id.camera_relative_layout);
+        preview.addView(mPreview);
 
     }
 
-    /** Called when the user taps the Profile button */
-    public void displayProfile(View view) {
-        Intent intent = new Intent(this, DisplayProfileActivity.class);
-        startActivity(intent);
-    }
 
-    /** Called when the user taps the FAQ button */
-    public void displayFAQ(View view) {
-        Intent intent = new Intent(this, DisplayFaqActivity.class);
-        startActivity(intent);
-    }
+    public void takePhoto() {
 
-    /** Called when the user taps the Practitioner button */
-    public void displayPractitioner(View view) {
-        Intent intent = new Intent(this, DisplayPractitionerActivity.class);
-        startActivity(intent);
-    }
-
-    /** Called when the user taps the Settings button */
-    public void displaySettings(View view) {
-        Intent intent = new Intent(this, DisplaySettingsActivity.class);
-        startActivity(intent);
-    }
-
-    public void takePhoto(View view) {
-
-        mCamera.takePicture(null, null, null, null);
+        mCamera.takePicture(null, null, mPicture);
 
         takePhotoButton.setVisibility(View.INVISIBLE);
         analyzeButton.setVisibility(View.VISIBLE);
@@ -142,12 +163,13 @@ public class MainActivity extends AppCompatActivity {
 
     }
 
-    public void retakePhoto(View view) {
-
-//        Log.i(TAG, "w = " + mCameraView.getWidth());
-//        Log.i(TAG, "h = " + mCameraView.getHeight());
+    public void retakePhoto() {
 
         mCamera.startPreview();
+
+        takePhotoButton.setVisibility(View.VISIBLE);
+        analyzeButton.setVisibility(View.INVISIBLE);
+        retakePhotoButton.setVisibility(View.INVISIBLE);
 
 
     }
