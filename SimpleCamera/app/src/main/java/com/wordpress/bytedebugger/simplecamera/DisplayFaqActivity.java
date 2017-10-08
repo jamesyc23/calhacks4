4b9d7dffc7d5d7932890bdd41e2137c8082e6b74
Elaine You <elaineyou97@gmail.com>
2017-10-07 17:41:28 -0700

    added activity navigtion

--- SimpleCamera/app/src/main/java/com/wordpress/bytedebugger/simplecamera/DisplayFaqActivity.java
+++ SimpleCamera/app/src/main/java/com/wordpress/bytedebugger/simplecamera/DisplayFaqActivity.java
@@ -1,7 +1,9 @@
 package com.wordpress.bytedebugger.simplecamera;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
 
 public class DisplayFaqActivity extends AppCompatActivity {
 
@@ -9,5 +11,11 @@ public class DisplayFaqActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_display_faq);
+    }
+
+    /** Called when the user taps the Home button */
+    public void displayMain(View view) {
+        Intent intent = new Intent(this, MainActivity.class);
+        startActivity(intent);
     }
 }
